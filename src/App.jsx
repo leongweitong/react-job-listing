74de241376a04d8aa530e6939d3ec25ae19c35c2
leongweitong <weitong0724@gmail.com>
2024-07-16 06:31:00 +0800

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import MainLayout from './layouts/MainLayout'
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<MainLayout /> }>
+			<Route index element={ <HomePage /> } />
+		</Route>
+	)
+)
 
 const App = () => {
-	const name = 'john'
-	const x = 10
-	const y =20
-
-	return (
-		<>
-			<h1 className="text-3xl font-normal underline">
-				Hello {name}!
-			</h1>
-			<p>sum of {x} and {y} is {x + y}</p>
-		</>
-	)
+  return <RouterProvider router={router} />
 }
 
 export default App
