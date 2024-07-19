ba1bba0b711e4d8dc4541b1e39164d51a402401c
leongweitong <weitong0724@gmail.com>
2024-07-20 00:33:39 +0800

    update confrim alert

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { useParams, useLoaderData, useNavigate } from 'react-router-dom'
-import { FaArrowLeft } from 'react-icons/fa'
+import { FaArrowLeft, FaMapMarked } from 'react-icons/fa'
+
 import { toast } from 'react-toastify'
 import { Link } from 'react-router-dom'
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 const JobPage = ({deleteJob}) => {
     const {id} = useParams()
@@ -10,27 +13,38 @@ const JobPage = ({deleteJob}) => {
     const navigate = useNavigate()
     
     const onDeleteClick = (id) => {
-        const confirm = window.confirm('Are you sure you want to delete this listing?')
+        confirmAlert({
+            title: 'Confirm to delete',
+            message: 'Are you sure you want to delete this job listing?',
+            buttons: [
+              {
+                label: 'Yes',
+                onClick: () => {
+                    deleteJob(id)
+                    toast.success('Job Deleted Successfully.')
+                    navigate('/jobs')
+                }
+              },
+              {
+                label: 'Cancel',
+                onClick: () => {
 
-        if(!confirm) return
-
-        deleteJob(id)
-
-        toast.success('Job Deleted Successfully.')
-
-        navigate('/jobs')
+                }
+              }
+            ]
+        });
     }
 
     return (
         <>
             <section>
                 <div className="container m-auto py-6 px-6">
-                <a
-                    href="/jobs"
+                <Link
+                    to="/jobs"
                     className="text-indigo-500 hover:text-indigo-600 flex items-center"
                 >
                     <FaArrowLeft className="mr-2" /> Back to Job Listings
-                </a>
+                </Link>
                 </div>
             </section>
 
@@ -45,13 +59,9 @@ const JobPage = ({deleteJob}) => {
                         <h1 className="text-3xl font-bold mb-4">
                         { job.title }
                         </h1>
-                        <div
-                        className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
-                        >
-                        <i
-                            className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
-                        ></i>
-                        <p className="text-orange-700">{ job.location }</p>
+                        <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+                            <FaMapMarked className='text-lg text-orange-700 mr-2' />
+                            <p className="text-orange-700">{ job.location }</p>
                         </div>
                     </div>
 
@@ -95,11 +105,11 @@ const JobPage = ({deleteJob}) => {
 
                     <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                         <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-                        <Link
-                        to={`/edit-job/${job.id}`}
-                        className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                        >Edit Job</Link
+                        <Link to={`/edit-job/${job.id}`} className="bg-indigo-500 hover:bg-indigo-600 
+                            text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                         >
+                            Edit Job
+                        </Link>
                         <button
                         onClick={() => onDeleteClick(job.id)}
                         className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
