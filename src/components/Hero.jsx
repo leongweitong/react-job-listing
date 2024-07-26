d0ed602092ec2fb794a0ec9a6067a2d0d317ba40
leongweitong <weitong0724@gmail.com>
2024-07-27 01:02:59 +0800

    heros images load from db.json

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,40 +1,75 @@
 import React, { useState, useEffect } from 'react'
-import image1 from '../assets/images/hero-1.jpeg'
-import image2 from '../assets/images/hero-2.jpeg'
-import image3 from '../assets/images/hero-3.png'
-
-const images = [image1, image2, image3];
+const allImages = import.meta.glob('../assets/images/*.jpeg')
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [isAnimating, setIsAnimating] = useState(true)
+    const [heros, setHeros] = useState([])
+    const [images, setImages] = useState({})
 
     useEffect(() => {
-        const handleAnimation = () => {
-          setIsAnimating(false);
-          setTimeout(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-            setIsAnimating(true);
-          }, 1000); // Match the duration of your fadeOutSlide animation
+        const loadImages = async () => {
+            const importedImages = {};
+            for (const img in allImages) {
+              const imageModule = await allImages[img]();
+              const imageName = img.replace('../assets/images/', '');
+              importedImages[imageName] = imageModule.default;
+            }
+            setImages(importedImages);
+          };
+      
+        loadImages();
+
+        const fetchHeros = async () => {
+          try {
+            const res = await fetch('/api/heros');
+            const data = await res.json();
+            setHeros(data);
+          } catch (error) {
+            console.error("Error fetching hero images:", error);
+          }
         };
     
-        const interval = setInterval(handleAnimation, 5000);
+        fetchHeros();
     
-        return () => clearInterval(interval);
-    }, []);
+        const handleAnimation = () => {
+          setIsAnimating(false);
+          const timeoutId = setTimeout(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % heros.length);
+            setIsAnimating(true);
+          }, 1000); // Match the duration of your fadeOutSlide animation
+    
+          return () => clearTimeout(timeoutId);
+        };
+    
+        const intervalId = setInterval(handleAnimation, 8000);
+    
+        return () => clearInterval(intervalId);
+    }, [heros.length]);
+
+    const getNextIndex = (index, step) => (index + step) % heros.length
     
     return (
         <section className="container-xl lg:container mx-auto px-4 pt-4">
             <div className='flex flex-row gap-2 md:gap-4 justify-between h-48 md:h-96 overflow-hidden relative'>
                 <div className='w-9/12 md:w-10/12 overflow-hidden'>
-                    <img src={images[currentIndex]} alt="" className={`h-full w-full object-cover rounded-lg transition transform ${isAnimating ? 'animate-fadeInSlide' : 'animate-fadeOutSlide'}`} />
+                {heros.length > 0 && (
+                    <img
+                    src={images[heros[currentIndex].name]}
+                    alt=""
+                    className={`h-full w-full object-cover rounded-lg ${isAnimating ? 'animate-fadeInSlide' : 'animate-fadeOutSlide'}`}
+                    />
+                )}
                 </div>
-                <div className='w-1/12 overflow-hidden'>
-                    <img src={images[(currentIndex + 1) % images.length]} alt="" className={`h-full w-full object-cover rounded-lg`} />
+                {heros.length > 0 && Array.from({ length: 2 }).map((_, i) => (
+                <div key={i} className='w-1/12 overflow-hidden'>
+                    <img
+                    src={images[heros[getNextIndex(currentIndex, i + 1)].name]}
+                    alt=""
+                    className="h-full w-full object-cover rounded-lg"
+                    />
                 </div>
-                <div className='w-1/12 overflow-hidden'>
-                    <img src={images[(currentIndex + 2) % images.length]} alt="" className={`h-full w-full object-cover rounded-lg`} />
-                </div>
+                ))}
             </div>
         </section>
     )
